5a06263e1380acc9c5c6952f85b5404aa38732ea
Alejandro Tomasella <aletomasella09@gmail.com>
2024-08-03 13:28:45 -0300

    feat: Adding views

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"html/template"
+	"io"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(write io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(write, name, data)
+}
+
+func newTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("../views/*.html")),
+	}
+}
+
+type IndexData struct {
+	Title string
+	Count int
+}
 
 func main() {
 	e := echo.New()
@@ -11,13 +33,18 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	e.Renderer = newTemplates()
+
+	indexData := IndexData{
+		Title: "Hello, World!",
+		Count: 0,
+	}
+
 	e.GET("/", func(c echo.Context) error {
 
-		jsonRepose := make(map[string]interface{})
+		indexData.Count++
+		return c.Render(200, "index", indexData)
 
-		jsonRepose["message"] = "Hello, World!"
-
-		return c.JSON(200, jsonRepose)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
